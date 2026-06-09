01c41bfc9523565406e37cc038564c5090927f0d
lets-make-ashwani <ashvish420waniwakarma.com>
2026-06-09 10:41:53 +0530

    implement skelton loading  timing

--- src/app/components/ui/skeleton.tsx
+++ src/app/components/ui/skeleton.tsx
@@ -4,7 +4,7 @@ function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="skeleton"
-      className={cn("bg-accent animate-pulse rounded-md", className)}
+      className={cn("bg-accent/80 animate-[pulse_1s_cubic-bezier(0.4,0,0.6,1)_infinite] rounded-md", className)}
       {...props}
     />
   );
